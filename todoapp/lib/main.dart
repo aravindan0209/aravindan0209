f92abec040cdb6bfe6d2e2aee468e034fe1173e9
aravindan0209 <84848856+aravindan0209@users.noreply.github.com>
2021-05-27 15:28:45 +0530

    Add files via upload

---
+++ todoapp/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:todoapp/todoui.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark().copyWith(
+        accentColor: Colors.purple,
+      ),
+      home: Todoui(),
+    );
+  }
+}
